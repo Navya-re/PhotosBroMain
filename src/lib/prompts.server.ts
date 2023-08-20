81fda0e5e774bc245bcbbe5c8e7015db6a31dae2
Navya-re <134678539+Navya-re@users.noreply.github.com>
2023-08-20 16:45:57 +0530

    Update prompts.server.ts

--- src/lib/prompts.server.ts
+++ src/lib/prompts.server.ts
@@ -9,10 +9,9 @@ export function getSubjectName() {
 }
 
 export const getRefinedInstanceClass = (instanceClass: string | null) => {
-	return instanceClass === 'man' ? 'male' : instanceClass === 'youngm' ? 'Young Male' : instanceClass === 'youngf' ? 'Young Female' : instanceClass === 'woman' ? 'female' : instanceClass;
-
+	return instanceClass === 'man' || instanceClass === 'woman' ? 'person' : instanceClass;
 };
 
 export function getReplacedPrompt(prompt: string, instanceClass: string | null) {
-	return prompt.replaceAll('@me', `${getSubjectName()} ${getRefinedInstanceClass(instanceClass)}`);
+	return prompt.replaceAll('@me', `${getRefinedInstanceClass(instanceClass)} ${getSubjectName()}`);
 }
