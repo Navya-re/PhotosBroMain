5ff7a49defc56eb523cfb3222334cd8796ce8bef
Navya-re <134678539+Navya-re@users.noreply.github.com>
2023-08-20 17:13:02 +0530

    Update replicate.server.ts

--- src/lib/replicate.server.ts
+++ src/lib/replicate.server.ts
@@ -40,7 +40,7 @@ async function getClient<T extends object>({
 }
 
 export const getRefinedInstanceClass = (instanceClass: string) => {
-	return instanceClass === 'man' ? 'male' : instanceClass === 'youngm' ? 'Young Male' : instanceClass === 'youngf' ? 'Young Female' : instanceClass === 'woman' ? 'female' : instanceClass;
+	return instanceClass === 'man' ? 'male' : instanceClass === 'woman' ? 'female' : instanceClass;
 };
 
 export const replacePromptToken = (prompt: string, instanceClass: string) => {
