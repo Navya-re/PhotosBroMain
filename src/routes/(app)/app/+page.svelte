46321de3bebb9294ea2a5e4ce806546efcd30957
Navya-re <134678539+Navya-re@users.noreply.github.com>
2023-08-31 19:40:56 +0530

    Update +page.svelte

--- src/routes/(app)/app/+page.svelte
+++ src/routes/(app)/app/+page.svelte
@@ -589,7 +589,7 @@
 			{/if}
 
 
-			{#if userInfo.trained && userInfo.counter < 50}
+			{#if !userInfo.trained && userInfo.counter < 50}
 <div class="form-control w-full max-w-xs">
   <label class="label">
     <span class="label-text text-inherit">Choose the style</span>
